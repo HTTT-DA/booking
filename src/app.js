891c0853c92805347c481d33789f6b0810e466ea
wander <anhkhoa24082001@gmail.com>
2023-04-14 16:33:02 +0700

    set up website: add public file, add header-footer for website

--- src/app.js
+++ src/app.js
@@ -1,23 +1,30 @@
 const createError = require('http-errors');
 const express = require('express');
-
+const session = require("express-session");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require("cors");
+const cors = require('cors');
 
-// const redis = require('./config/connect-to-redis.js');
-// const cassandra = require('./config/connect-to-cassandra');
-// const mongodb = require('./config/connect-to-mongodb');
+const indexRouter = require('./router/router');
+
+/*const redis = require('./config/connect-to-redis.js');
+const cassandra = require('./config/connect-to-cassandra');
+const mongodb = require('./config/connect-to-mongodb');
 const mssql = require('./config/connect-to-mssql');
-const usersRouter = require("./api/user/user-router");
+const usersRouter = require("./components/user/user-router");
 
-/*redis.connect().then(r => console.log('Redis connected'));
+redis.connect().then(r => console.log('Redis connected'));
 cassandra.connect().then(c => console.log('Cassandra connected'));
-mongodb.connect().then(m => console.log('MongoDB connected'));*/
-mssql.getConnection()
+mongodb.connect().then(m => console.log('MongoDB connected'));
+mssql.getConnection()*/
 
 const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, "components")]);
+app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,7 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
