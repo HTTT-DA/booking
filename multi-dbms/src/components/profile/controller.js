e349d0414842f8b720951d671039de7e34710c55
buicongdanh <64085482+buicongdanh@users.noreply.github.com>
2023-05-08 22:06:34 +0700

    file delete

--- multi-dbms/src/components/profile/controller.js
+++ multi-dbms/src/components/profile/controller.js
@@ -1,6 +1,6 @@
 exports.render = async (req, res) => {
     try {
-        res.render("profile/views/profile")
+        res.render("profile/views/profile", { testData: "test"})
     } catch (err) {
         res.json(err.message);
     }
