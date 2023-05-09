c313b020745ad34a63dae7e3f48da5e81a74e0d0
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-05-09 20:52:20 +0700

    Reservation - Get quantity from user
    Done

--- multi-dbms/src/components/property/controller.js
+++ multi-dbms/src/components/property/controller.js
@@ -4,7 +4,6 @@ const {paging} = require("../../public/js/paging");
 
 exports.searchProperty = async (req, res) => {
     try {
-
         const result = await Property.searchProperty(req.query);
         res.render("property/views/property-search", { properties: result, paging: paging(req.query.page)})
     } catch (err) {
@@ -19,7 +18,7 @@ exports.getPropertyDetail = async (req, res) => {
         property.questions = property.questions.slice(0,3);
 
         const accommodations = await Property.getAccommodationOfProperty(req.params.id);
-        for(let i = 0; i < accommodations.length; i++){
+        for (let i = 0; i < accommodations.length; i++){
             accommodations[i].price_after_sale_off = accommodations[i].price - accommodations[i].price * accommodations[i].sale_off / 100;
         }
 
@@ -27,4 +26,52 @@ exports.getPropertyDetail = async (req, res) => {
     } catch (err) {
         res.json(err.message);
     }
+}
+
+exports.getReserveDetail = async (req, res) => {
+    try {
+      const property_temp = await Property.getPropertyDetail(req.params.id);
+      const property = {
+        'property_id': property_temp.property_id,
+        'property_name': property_temp.name
+      };
+      const accommodations_temp = await Property.getAccommodationOfProperty(req.params.id);
+      const accommodations = [
+        {
+          accommodation_id: accommodations_temp[0]._id,
+          price: accommodations_temp[0].price,
+          price_sale_off:
+            accommodations_temp[0].price -
+            (accommodations_temp[0].price * accommodations_temp[0].sale_off) /
+              100,
+        },
+        {
+          accommodation_id: accommodations_temp[1]._id,
+          price: accommodations_temp[1].price,
+          price_sale_off:
+            accommodations_temp[1].price -
+            (accommodations_temp[1].price * accommodations_temp[1].sale_off) /
+              100,
+        },
+        {
+          accommodation_id: accommodations_temp[2]._id,
+          price: accommodations_temp[2].price,
+          price_sale_off:
+            accommodations_temp[2].price -
+            (accommodations_temp[2].price * accommodations_temp[2].sale_off) /
+              100,
+        },
+      ];
+
+      const quantity_1 = +req.body.quantity_number_0;
+      const quantity_2 = +req.body.quantity_number_1;
+      const quantity_3 = +req.body.quantity_number_2;
+
+      for (let i = 0; i < 3; i++) {
+        
+      }
+    } catch (err) {
+      res.json(err.message);
+    }
+
 }
