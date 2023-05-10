45e17dceacf9a866c88443e82d7ae2678b21b290
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-05-10 17:13:07 +0700

    Complete Reservation
    Done

--- multi-dbms/src/components/property/model.js
+++ multi-dbms/src/components/property/model.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const propertyDetailModel = mongoose.model('PropertyDetail', {}, 'PropertyDetail');
 const accommodationModel = mongoose.model('Accommodation', {}, 'Accommodation');
 
+const connect = require("../../config/connect-to-redis");
+
 module.exports = function () {
     this.searchProperty = async (search) => {
         try {
@@ -61,5 +63,75 @@ module.exports = function () {
             return err.message;
         }
     };
+
+    this.addPropertyToRedis = async (user_id, booking_info, accommodations) => {
+        try {
+          const client = await connect.connect();
+          const keyHMSET = "Room:" + user_id;
+          const keyZADD = "Accommodations:" + user_id;
+          const membersToAdd = [
+            1,
+            accommodations[0].quantity,
+            2,
+            accommodations[1].quantity,
+            3,
+            accommodations[2].quantity,
+          ];
+          // Property
+          await client.hmset(
+            keyHMSET,
+            "property_id",
+            booking_info.property_id,
+            "property_name",
+            booking_info.property_name,
+            "price",
+            booking_info.price_sale_off,
+            "day_start",
+            booking_info.date_start,
+            "day_end",
+            booking_info.date_end,
+
+            (err, result) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log("HMSET success");
+              }
+            }
+          );
+          // Accommodations
+          await client.exists(keyZADD, (err, reply) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            if (reply === 1) {
+              // key tồn tại, xóa tất cả các member
+              client.zremrangebyrank(keyZADD, 0, -1, (err, result) => {
+                if (err) {
+                  console.error(err);
+                } else {
+                  console.log("Zremrangebyrank success");
+                }
+              });
+            } else {
+              // key không tồn tại
+              console.log(`Key does not exist`);
+            }
+          });
+          // Add members to sorted set
+          await client.zadd(keyZADD, membersToAdd, (err, response) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`zadd successfully`);
+            }
+            // Close Redis client connection
+            client.quit();
+          });
+        } catch (err) {
+            return err.message;
+        } 
+    }
 };
 
